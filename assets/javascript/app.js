8e1ec9fe02343a17021a7f4fa03e4b6339057aea
Lan Ejiri <lan.ejiri@yahoo.com>
2018-03-21 15:04:12 -0400

    some changes from desktop compooper

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var everything = [
+    var storeQuestions = [
 
         {question: 'What was the name of the AI computer in 2001: A Space Odessy?' ,
         answers: [ 'HAL 9000', 'Watson', 'Spacey', 'Siri'],
@@ -13,7 +13,21 @@ $(document).ready(function(){
         {question: 'An AI robot named Ava murders her creator in which film?' ,
         answers: [ 'ex Machina', 'iRobot', 'District 9', 'Avatar'],
         correctAnswer: 'ex Machina'},
+
+        {question: 'Which movie has an alien species affectionately named "Prawn"?' ,
+        answers: [ 'District 9', 'Alien', 'Interstellar', 'The Day the Earth Stood Still'],
+        correctAnswer: 'District 9'},
     ];
+
+    //makes sure every time refreh is pressed, question appear in different order
+    function randomizeQuestions() {
+        var randomQuestions = everything.sort(function(a, b){return 0.5 - Math.random()});
+        
+        $("#questionsgohere").append(randomQuestions[0].question);
+    }
+
+
+    randomizeQuestions();
 
 
     // for ( i = 0; i < everything.length; i++) {
@@ -23,10 +37,6 @@ $(document).ready(function(){
 
     // }   
     
-    function randomizeQuestions() {
-        everything.sort(function(a, b){return 0.5 - Math.random()});
-        
-        $("#questionsgohere").append(everything[0].question);
-    }
+    
 
 });
